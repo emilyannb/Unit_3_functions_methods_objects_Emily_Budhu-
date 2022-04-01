6597e5433c5e5f2c447051848db85ea997ca2f02
Emily Budhu <97969416+emilyannb@users.noreply.github.com>
2022-04-01 14:36:06 +0000

    js

--- basic-function.js
+++ basic-function.js
@@ -1,5 +1,5 @@
 var msg = 'Sign up to recieve our newsletter for 10% off!';
-function updateMessage(){
+function updateMessage() {
   var el = document.getElementById('message');
   el.textContent = msg;
 }
